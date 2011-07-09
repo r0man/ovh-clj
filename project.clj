ac4e16ab293522dca770c36e07789bc6b3a29b6c
Roman <roman@burningswell.com>
2011-07-09 14:45:38 +0200

    Initial import.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject ovh "0.0.1-SNAPSHOT"
+  :description "Clojure Library for the OVH Web Services"
+  :dependencies [[axis/axis "1.4"]
+                 [org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[lein-axis "0.1.1"]]
+  :axis [["http://www.ovh.com/soapi/soapi-re-1.24.wsdl" "com.ovh"]]
+  :java-source-path "src/java")
+
