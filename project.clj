4966d5a07374d30ed06b9c2fd2216026025310a9
Roman <roman@burningswell.com>
2011-09-17 17:30:27 +0200

    Added clojure source path.

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
                  [org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[lein-axis "0.1.1"]]
   :axis [["http://www.ovh.com/soapi/soapi-re-1.24.wsdl" "com.ovh"]]
+  :source-path "src/clojure"
   :java-source-path "src/java")
 
