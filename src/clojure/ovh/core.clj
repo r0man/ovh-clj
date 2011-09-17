3f08f5ed0e432ec26d4bc055a947d01f68338663
Roman <roman@burningswell.com>
2011-09-17 23:44:21 +0200

    Added the find-zone-entry, delete-zone-entry and update-zone-entry fns.

--- src/clojure/ovh/core.clj
+++ src/clojure/ovh/core.clj
@@ -1,11 +1,8 @@
 (ns ovh.core
-  (:use [clojure.contrib.def :only (defvar)]))
+  (:use [clojure.string :only (blank?)]))
 
-(defvar *service* nil
-  "The current OVH manager service.")
-
-(defvar *session* nil
-  "The current OVH manager session.")
+(def ^{:dynamic true} *service*)
+(def ^{:dynamic true} *session*)
 
 (defn service
   "Returns an OVH manager service."
@@ -20,13 +17,52 @@
   [domain] (bean (.domainInfo *service* *session* domain)))
 
 (defn zone-entry-list
-  "Returns the zone entries for the domain."
+  "Returns the zone entries for a domain."
   [domain] (map bean (seq (.zoneEntryList *service* *session* domain))))
 
+(defn find-zone-entry
+  "Find a zone entry by domain, subdomain and fieldtype. "
+  [domain subdomain fieldtype]
+  (->> (zone-entry-list domain)
+       (filter #(and (= subdomain (:subdomain %1)) (= fieldtype (:fieldtype %1))))
+       (first)))
+
 (defn add-zone-entry
-  "Add a zone entry for the domain."
-  [domain sub-domain type target & [overwrite]]
-  (.zoneEntryAdd *service* *session* domain sub-domain type target (or overwrite false)))
+  "Add a zone entry for a domain."
+  [domain subdomain fieldtype target & [overwrite]]
+  (.zoneEntryAdd *service* *session* domain subdomain fieldtype target (or overwrite false))
+  (find-zone-entry domain subdomain fieldtype))
+
+(defn delete-zone-entry
+  "Delete a zone entry for a domain."
+  [domain subdomain fieldtype]
+  (if-let [entry (find-zone-entry domain subdomain fieldtype)]
+    (.zoneEntryDel *service* *session* domain subdomain fieldtype (:target entry))))
+
+(defn update-zone-entry
+  "Update a zone entry for a domain."
+  [domain subdomain fieldtype target]
+  (if-let [entry (find-zone-entry domain subdomain fieldtype)]
+    (.zoneEntryModify *service* *session* domain subdomain fieldtype (:target entry) target)
+    (add-zone-entry domain subdomain fieldtype target))
+  (find-zone-entry domain subdomain fieldtype))
+
+(defn print-zone-entries
+  "Print the zone entries of domain."
+  [domain]
+  (println "SUBDOMAIN                      FIELDTYPE  TARGET")
+  (println "-----------------------------------------------------------------------------------------------------")
+  (doseq [entry (sort-by :subdomain (zone-entry-list domain))]
+    (println (format "%-30s %-10s %-30s" (:subdomain entry) (:fieldtype entry) (:target entry)))))
+
+(defn print-zone-entries
+  "Print the zone entries of domain."
+  [domain]
+  (let [template "%-15s %-10s %-30s"]
+    (println (format template "SUBDOMAIN" "FIELDTYPE" "TARGET"))
+    (println (apply str (repeat 80 "-")))
+    (doseq [entry (sort-by :subdomain (zone-entry-list domain))]
+      (println (format template (:subdomain entry) (:fieldtype entry) (:target entry))))))
 
 (defn login
   "Login with NIC handle and password and return a session."
